816126062188cde3f27831caf9be6fc58a5a0695
Cypher <penbeatboy@gmail.com>
2018-03-20 22:38:07 -0700

    Swipher

--- SWIPHER/main.py
+++ SWIPHER/main.py
@@ -1,10 +1,10 @@
 """
-d8888b. db    db d888888b db   db  .d8b.  d8b   db db    db   d888888b
-88  `8D `8b  d8' `~~88~~' 88   88 d8' `8b 888o  88 `8b  d8'     `88'
-88oooY'  `8bd8'     88    88ooo88 88ooo88 88V8o 88  `8bd8'       88
-88~~~b.    88       88    88~~~88 88~~~88 88 V8o88    88         88
-88   8D    88       88    88   88 88   88 88  V888    88        .88.
-Y8888P'    YP       YP    YP   YP YP   YP VP   V8P    YP      Y888888P
+d8888b. d88888b d888888b db   db  .d8b.  d8b   db db    db   d888888b
+88  `8D 88'     `~~88~~' 88   88 d8' `8b 888o  88 `8b  d8'     `88'
+88oooY' 88ooooo    88    88ooo88 88ooo88 88V8o 88  `8bd8'       88
+88~~~b. 88~~~~~    88    88~~~88 88~~~88 88 V8o88    88         88
+88   8D 88.        88    88   88 88   88 88  V888    88        .88.
+Y8888P' Y88888P    YP    YP   YP YP   YP VP   V8P    YP      Y888888P
 
 
 db       .d88b.  db    db d88888b   db    db  .d88b.  db    db
@@ -13,6 +13,7 @@ db       .d88b.  db    db d88888b   db    db  .d88b.  db    db
 88      88    88 `8b  d8' 88~~~~~      88    88    88 88    88
 88booo. `8b  d8'  `8bd8'  88.          88    `8b  d8' 88b  d88
 Y88888P  `Y88P'     YP    Y88888P      YP     `Y88P'  ~Y8888P'
+
 
 """
 
